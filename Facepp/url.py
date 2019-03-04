11da29f3b594b8040d6fc52d0a0d303a0ba4162a
hsq <hsqyc@163.com>
2019-03-04 22:38:28 +0800

    用face_token参数compare失败

--- Facepp/url.py
+++ Facepp/url.py
@@ -9,7 +9,7 @@ def url_detect(filepath):
     http_url = 'https://api-cn.faceplusplus.com/facepp/v3/detect'
     key = "q77EK-wF9dqlqqZAJKWEml_qki2GNTS1"
     secret = "WhXup4yLMc8OQF99mQ90DlV7p-hfYe46"
-    filepath = "./picture/" + filepath
+    filepath = "../img/" + filepath
     boundary = '----------%s' % hex(int(time.time() * 1000))
     data = []
     data.append('--%s' % boundary)
@@ -56,12 +56,17 @@ def url_detect(filepath):
         print(e.read().decode('utf-8'))
 
 
-def url_compare(filepath1, filepath2):
-    http_url = 'https://api-cn.faceplusplus.com/facepp/v3/detect'
-    key = "q77EK-wF9dqlqqZAJKWEml_qki2GNTS1"
-    secret = "WhXup4yLMc8OQF99mQ90DlV7p-hfYe46"
-    filepath1 = "./picture/" + filepath1
-    filepath2 = "./picture/" + filepath2
+def url_compare(filepath1, filepath2, face_token1=None, face_token2=None):
+    http_url = 'https://api-cn.faceplusplus.com/facepp/v3/compare'
+    # 免费
+    # key = "q77EK-wF9dqlqqZAJKWEml_qki2GNTS1"
+    # secret = "WhXup4yLMc8OQF99mQ90DlV7p-hfYe46"
+    # 收费
+    key = 'lMA4mtztEo3rqCA1IKDBYPhX_w3fWvY4'
+    secret = 'c8xugTC1ECKYM1K_FPjPWeNZuqEJZUFU'
+
+    filepath1 = "../img/" + filepath1
+    filepath2 = "../img/" + filepath2
     boundary = '----------%s' % hex(int(time.time() * 1000))
     data = []
     data.append('--%s' % boundary)
@@ -71,19 +76,26 @@ def url_compare(filepath1, filepath2):
     data.append('Content-Disposition: form-data; name="%s"\r\n' % 'api_secret')
     data.append(secret)
     data.append('--%s' % boundary)
-    fr = open(filepath1, 'rb')
-    data.append('Content-Disposition: form-data; name="%s"; filename=" "' % 'image_file1')
-    data.append('Content-Type: %s\r\n' % 'application/octet-stream')
-    data.append(fr.read())
-    fr.close()
-    data.append('--%s' % boundary)
-    fr = open(filepath2, 'rb')
-    data.append('Content-Disposition: form-data; name="%s"; filename=" "' % 'image_file2')
-    data.append('Content-Type: %s\r\n' % 'application/octet-stream')
-    data.append(fr.read())
-    fr.close()
-
-    data.append('--%s--\r\n' % boundary)
+    if face_token1 != None:
+        data.append('Content-Disposition: form-data; name="%s"\r\n' % 'face_token1')
+        data.append(face_token1)
+        data.append('--%s' % boundary)
+        data.append('Content-Disposition: form-data; name="%s"\r\n' % 'face_token2')
+        data.append(face_token1)
+        data.append('--%s' % boundary)
+    else:
+        fr = open(filepath1, 'rb')
+        data.append('Content-Disposition: form-data; name="%s"; filename=" "' % 'image_file1')
+        data.append('Content-Type: %s\r\n' % 'application/octet-stream')
+        data.append(fr.read())
+        fr.close()
+        data.append('--%s' % boundary)
+        fr = open(filepath2, 'rb')
+        data.append('Content-Disposition: form-data; name="%s"; filename=" "' % 'image_file2')
+        data.append('Content-Type: %s\r\n' % 'application/octet-stream')
+        data.append(fr.read())
+        fr.close()
+        data.append('--%s--\r\n' % boundary)
 
     for i, d in enumerate(data):
         if isinstance(d, str):
@@ -107,7 +119,12 @@ def url_compare(filepath1, filepath2):
     except urllib.error.HTTPError as e:
         print(e.read().decode('utf-8'))
 
+
 if __name__ == '__main__':
     # {"image_id": "9cfaRGDXY/39z7rqFiccMw==", "request_id": "1551685651,88575546-b3ac-4c1d-bb5a-cfc1c8160b2e", "time_used": 1362, "faces": [{"face_rectangle": {"width": 158, "top": 1616, "left": 1776, "height": 158}, "face_token": "792d9783dc6889ba53b9006c4386fbc0"}]}
-    url_detect('IMG_1676.JPG')
+    # url_detect('t4.jpg')
+    # t1 c35a0e984f8ddb7e8042de975ef495f9
+    # t2 1c038cb000067e5fa8eaa1e8209ff597
+    # ?2 53e4431701bfab9bb49c746669724c17
+    url_compare('t1.jpg', 't2.jpg', 'c35a0e984f8ddb7e8042de975ef495f9', 'c35a0e984f8ddb7e8042de975ef495f9')
     exit()
